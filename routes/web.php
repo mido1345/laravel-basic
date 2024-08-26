a17b890661424b15c79d5a37622339a32c0ab50c
‘mido1345’ <mido.cla.v3v@gmail.com>
2024-08-26 18:08:57 +0900

    更新

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// ルーティングを設定するコントローラを宣言する
+use App\Http\Controllers\HelloController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\VendorController;
+use App\Http\Controllers\RequestController;
+use App\Http\Controllers\ResponseController;
+use App\Http\Controllers\SignInController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +24,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return 'Hello, world!';
-});
+Route::get('/hello', [HelloController::class, 'index']);
+
+Route::get('/products', [ProductController::class, 'index']);
+
+Route::get('/products/create', [ProductController::class, 'create']);
+ 
+Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
+
+Route::get('/products/{id}', [ProductController::class, 'show']);
+
+Route::get('/vendors/create', [VendorController::class, 'create']);
+ 
+Route::post('/vendors/store', [VendorController::class, 'store'])->name('vendors.store');
+
+
+Route::get('/vendors/{id}', [VendorController::class, 'show']);
+
+Route::get('/requests/create', [RequestController::class, 'create']);
+ 
+Route::post('/requests/confirm', [RequestController::class, 'confirm'])->name('requests.confirm');
+
+Route::get('/responses', [ResponseController::class, 'index']);
+
+Route::get('/sign-in', [SignInController::class, 'create']);
+ 
+Route::post('/sign-in', [SignInController::class, 'store'])->name('sign-in.store');
